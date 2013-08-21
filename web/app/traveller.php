5c87e2f52131fac6ecd600094b4fecb776973413
chan4lk <chan4lk@gmail.com>
2013-08-21 21:52:52 +0530

    restfull api added

---
+++ web/app/traveller.php
@@ -0,0 +1,12 @@
+<?php
+	echo json_encode(array(
+		'name' => 'name',
+		'scrN' => 'screen_name',
+		'eml' => 'user@domain.com',
+		'urTy' => array('tourist','service provider'),
+		'dId' => 'device_id',
+		'_id' => 'email',
+		'likes' => array(
+		)
+	));
+?>
---
+++ web/class/User.php
@@ -0,0 +1,11 @@
+<?php
+class User{			
+			public function __construct($urN, $scrn, $mail,$urTy, $did){
+				$this->urN = $urN;
+				$this->scrn = $scrn;
+				$this->mail = $mail;
+				$this->urTy = $urTy;	
+				$this->did  = $did;
+			}
+		}//end class
+?>
