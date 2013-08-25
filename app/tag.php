0444cbaeb7148a257c88ded23a46b1ea9568569b
Chandima Ranaweera <chan@nspiron-N5110.(none)>
2013-08-25 19:33:24 +0530

    web interface added

--- app/tag.php
+++
@@ -1,7 +0,0 @@
-<?php
-	echo json_encode(array(
-		'tgTy' => 'tag_type',
-        'tgN' => 'tag_name',
-		'gph' => 'graph'
-	));
-?>
--- web/app/trip.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo json_encode(array(
-		'star' => 'star_time',
-        'end' => 'end_time',
-        'scrN' => 'screen_name',
-		'tag' => array()
-	));
-?>
