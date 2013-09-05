d89bd887e7ca47f963bb5f76e457381cf8034e70
badtux <kalinga@gmail.com>
2013-09-05 18:04:41 +0530

    added default, users ctrls

---
+++ app/users.ctrl.php
@@ -0,0 +1,22 @@
+<?php
+	require_once 'class/User.php';
+
+	if(__ROUTER_PATH == '/users/add'){
+		try{
+			$n = new User();
+			$name = $screenName = $email = null;
+			extract($_POST,EXTR_IF_EXISTS);
+			$n->setName($name)->setEmail($email)->setScreenName($screenName)->update();
+		}
+		catch(Exception $e){
+			echo json_encode(array(
+				'code' => $e->getCode(),
+				'msg' => $e->getMessage()
+			));
+		}
+	}
+
+	if(__ROUTER_PATH == '/users/find'){
+		User::find($screenName);
+	}
+?>
