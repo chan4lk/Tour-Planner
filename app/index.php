951301fac5580e2f9ae0f6d515a7d61db5139862
chan4lk <chan4lk@gmail.com>
2013-10-22 15:37:32 +0530

    new changes for urls

--- app/index.php
+++ app/index.php
@@ -1,14 +1,26 @@
 <?php
+	require_once __DIR__.'/class/mongo.class.php';
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+	
+	
+
 	define('__ROUTER_PATH', '/'.trim((string)parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH),'/'));
-
-	$conn = new MongoClient("mongodb://chan4lk:chan4cha2@dharma.mongohq.com:10075/traveller");
-	define(__DS, $conn->selectDB('traveller'));
-
-	switch (__ROUTER_PATH){
+	define('__METHOD', $_SERVER['REQUEST_METHOD']);	
+	switch (__ROUTER_PATH) {
+		case '/':
+			echo "we are on root";
+			break;
+			case '/rock':
+				header("Location:/rockmongo");
 		case (substr(__ROUTER_PATH,0,10) == '/users/get'):
-		case '/users/add':
-		case '/users/find':	require_once 'users.ctrl.php';	break;
-
-		case '/':	require_once 'default.ctrl.php';	break;
+		case (substr(__ROUTER_PATH,0,11) == '/users/find'):
+        case '/users/add':
+        case '/users/find':     require_once __DIR__.'/ctrl/users.ctrl.php';  
+        	break;
+		default:
+			echo __ROUTER_PATH;
+			break;
 	}
 ?>
+
